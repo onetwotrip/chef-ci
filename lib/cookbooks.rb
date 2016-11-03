31c4d9f26dfd9f1870345caa3e6c163e50046444
Dmitry Shmelev <avikez@gmail.com>
2016-11-03 11:03:46 +0300

    Add colorize logger; Fix git fetch; Skip deploy

--- lib/cookbooks.rb
+++ lib/cookbooks.rb
@@ -5,7 +5,7 @@ class Cookbooks
     system 'git rev-parse --is-inside-work-tree'
     system "git config remote.origin.url #{url}"
     system 'git -c core.askpass=true fetch --tags --progress git@gitlab.twiket.com:chef/chef.git \
-            +refs/heads/*:refs/remotes/origin/* +refs/merge-requests/*/head:refs/remotes/origin/merge-requests/*'
+            +refs/heads/*:refs/remotes/origin/*'
     system "git checkout -f #{commit}"
     `git diff --name-only origin/develop`.split(/\n/).grep(%r{cookbooks/(.*)/}).map { |f| f.match(%r{cookbooks/([a-z\-_]*)/})[1] }.uniq
   end
