1cd20a24a979c07c1618536e4000aac37b51bde3
Dmitry Shmelev <avikez@gmail.com>
2016-11-24 16:50:05 +0300

    Fix binaries

--- lib/common/const.rb
+++
@@ -1,6 +0,0 @@
-require 'pathname'
-APP_PATH = File.expand_path('../../', Pathname.new(__FILE__).realpath)
-LOG_PATH = "#{APP_PATH}/logs".freeze
-GEM_FILE = "#{APP_PATH}/Gemfile".freeze
-BOOTSTRAP_TEMPLATE = "#{APP_PATH}/templates/twiket-bootstrap".freeze
-ENV['BUNDLE_GEMFILE'] ||= GEM_FILE
--- lib/common_libs.rb
+++ lib/common_libs.rb
@@ -1,3 +1,11 @@
+require 'pathname'
+APP_PATH = File.expand_path('../../', Pathname.new(__FILE__).realpath)
+LOG_PATH = "#{APP_PATH}/logs".freeze
+GEM_FILE = "#{APP_PATH}/Gemfile".freeze
+BOOTSTRAP_TEMPLATE = "#{APP_PATH}/templates/twiket-bootstrap".freeze
+ENV['BUNDLE_GEMFILE'] ||= GEM_FILE
+require 'bundler/setup'
+
 require 'chef/config'
 require 'chef/knife'
 require 'colorize'
@@ -5,7 +13,6 @@ require 'fileutils'
 require 'mixlib/cli'
 require 'simple_config'
 
-require 'common/const'
 require 'common/logger'
 
 require 'knife/cookbooks'
